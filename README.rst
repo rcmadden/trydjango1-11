6124c1743fc6fbbe18e8177e8f12d90de9ffbfeb
Russia Madden <dataengineer007@gmail.com>
2017-09-14 02:08:13 +0000

    template rendering and context

--- README.rst
+++ README.rst
@@ -1,3 +1,6 @@
 # trydjango1-11
 run notebook server on cloud9
 jupyter notebook --ip=0.0.0.0 --port=8080 --no-browser
+
+# in src/
+python manage.py runserver $IP:$PORT
--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,11 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import random
 
 # Create your views here.
 def home(request):
-    context = {
-        'html_var':'f strings'
+    bool_item = True
+    num = None
+    some_list = (random.randint(0, 10000), random.randint(0, 10000), random.randint(0, 10000))
+    # logic in view instead of template
+    if bool_item:
+        num = random.randint(0, 10000000)
         
+    context = {
+        'bool_item': bool_item,
+        'html_var':'f strings',
+        'num': num,
+        'some_list': some_list
     }
     
     # return HttpResponse(html_) # another way to retun a response
