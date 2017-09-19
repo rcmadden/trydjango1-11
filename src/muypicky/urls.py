f2c026b1adf0f70fd47304c1303eb9df333fe52a
Russia Madden <dataengineer007@gmail.com>
2017-09-19 23:28:35 +0000

    ListView basic

--- src/muypicky/urls.py
+++ src/muypicky/urls.py
@@ -17,12 +17,19 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
-from restaurants.views import restaruant_listview
+from restaurants.views import (
+    restaruant_listview,
+    RestaurantListView,
+    MexianRestaurantListView,
+    AsianFusionRestaurantListView
+    )
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='home.html')), 
-    url(r'^restaurants/$', restaruant_listview),
+    url(r'^restaurants/$', RestaurantListView.as_view()),
+    url(r'^restaurants/mexican/$', MexianRestaurantListView.as_view()),
+    url(r'^restaurants/asian/$', AsianFusionRestaurantListView.as_view()),
     url(r'^about/$', TemplateView.as_view(template_name='about.html')),
     url(r'^more.info/$', TemplateView.as_view(template_name='more.info.html')),
     url(r'contact/$', TemplateView.as_view(template_name='contact.html'))
--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
 from .models import RestaurantLocation
+
 # Create your views here.
 def restaruant_listview(request):
     template_name = 'restaurants/restaruants_list.html'
@@ -13,5 +14,16 @@ def restaruant_listview(request):
     }
     return render(request, template_name, context)
 
-        
+class RestaurantListView(ListView):
+    queryset = RestaurantLocation.objects.all()
+    template_name = 'restaurants/restaruants_list.html'
+
+    
+class MexianRestaurantListView(ListView):
+    queryset = RestaurantLocation.objects.filter(category__iexact='mexican')
+    template_name = 'restaurants/restaruants_list.html'
+
+class AsianFusionRestaurantListView(ListView):
+    queryset = RestaurantLocation.objects.filter(category__iexact='asian fusion')
+    template_name = 'restaurants/restaruants_list.html'
     
