e7b768137a4aa87c2a14fb4abf5f2173a7e9309e
Russia Madden <dataengineer007@gmail.com>
2017-09-21 01:33:29 +0000

    Add Slug field and Unique Slug Generator

--- src/restaurants/models.py
+++ src/restaurants/models.py
@@ -7,7 +7,15 @@ class RestaurantLocation(models.Model):
      category   = models.CharField(max_length=120, null=True, blank=True)
      timestamp  = models.DateTimeField(auto_now=True)
      updated    = models.DateTimeField(auto_now=True)
+     # slug       = models.SlugField(unique=True)
+     slug       = models.SlugField(null=True, blank=True)
+     
+     
      # replaces object name in django admin with field name
      def __str__(self):
          return self.name
      
+     @property
+     def title(self):
+          return self.name  # so we can use obj.title
+          
