7a4678d89b6c94fa000a98850a285b2abb356482
Russia Madden <dataengineer007@gmail.com>
2017-09-23 23:22:46 +0000

    Form Field Limits with QuerySet for User Items

--- src/menus/models.py
+++ src/menus/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.core.urlresolvers import reverse
 
 from restaurants.models import RestaurantLocation
 
@@ -8,12 +9,16 @@ from restaurants.models import RestaurantLocation
 class Item(models.Model):
     user        = models.ForeignKey(settings.AUTH_USER_MODEL)
     restaurant  = models.ForeignKey(RestaurantLocation)
-    name         = models.CharField(max_length=120)
+    name        = models.CharField(max_length=120)
     contents    = models.TextField(help_text='Seperate each item by comma')
     excludes    = models.TextField(blank=True, null=True, help_text='Seperate each item by comma')
     public      = models.BooleanField(default=True)
     timestamp   = models.DateTimeField(auto_now_add=True)
-    updated      = models.DateTimeField(auto_now=True)
+    updated     = models.DateTimeField(auto_now=True)
+    
+    def get_absolute_url(self):
+        return reverse('menus:detail', kwargs={'pk':self.pk})
+
     
     class Meta:
         ordering = ['-updated', '-timestamp'] # Item.objects.all() most recent updated item first
--- src/muypicky/urls.py
+++ src/muypicky/urls.py
@@ -13,16 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
 
 # source:  class LoginView https://docs.djangoproject.com/en/1.11/topics/auth/default/#django.contrib.auth.views.LoginView
 # from django.contrib.auth import views as auth_views
 from django.contrib.auth.views import LoginView
-
-
-from django.contrib.auth import views as auth_views
+# from django.contrib.auth import views as auth_views
 
 from restaurants.views import (
     restaurant_createview,
@@ -31,17 +29,17 @@ from restaurants.views import (
     RestaurantDetailView,
     RestaurantCreateView
     )
+    
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name='home.html')), 
-    # url(r'^accounts/login/$', auth_views.LoginView.as_view()),
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'), 
     url(r'^login/$', LoginView.as_view(), name='login'),
-    url(r'^restaurants/$', RestaurantListView.as_view()),
-    url(r'^restaurants/create/$', RestaurantCreateView.as_view()),
-    # url(r'^restaurants/create/$', restaurant_createview),
-    url(r'^restaurants/(?P<slug>[\w-]+)/$', RestaurantDetailView.as_view()),
-    # url(r'^restaurants/(?P<slug>[\w-]+)/$', restaurant_createview),
-    url(r'^about/$', TemplateView.as_view(template_name='about.html')),
+    # url(r'^restaurants/$', RestaurantListView.as_view()),
+    url(r'^restaurants/',include('restaurants.urls', namespace='restaurants')),
+    # url(r'^restaurants/create/$', RestaurantCreateView.as_view()),
+    # url(r'^restaurants/(?P<slug>[\w-]+)/$', RestaurantDetailView.as_view()),
+    url(r'^items/', include('menus.urls', namespace='menus')),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
     url(r'^more.info/$', TemplateView.as_view(template_name='more.info.html')),
-    url(r'contact/$', TemplateView.as_view(template_name='contact.html')),
+    url(r'contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
 ]
