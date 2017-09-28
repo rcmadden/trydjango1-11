6f9010663b0251fba6e0bfdbd3db708f074d3c29
Russia Madden <dataengineer007@gmail.com>
2017-09-28 04:06:31 +0000

    Activation Keys

--- src/muypicky/urls.py
+++ src/muypicky/urls.py
@@ -1,35 +1,18 @@
-"""muypicky URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
 
-# source:  class LoginView https://docs.djangoproject.com/en/1.11/topics/auth/default/#django.contrib.auth.views.LoginView
 
 from django.contrib.auth.views import LoginView, LogoutView
-# https://docs.djangoproject.com/en/1.11/topics/auth/default/#django.contrib.auth.views.LogoutView
 
 from menus.views import HomeView
-from profiles.views import ProfileFollowToggle, RegisterView
+from profiles.views import ProfileFollowToggle, RegisterView, activate_user_view
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    # url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^$', HomeView.as_view(), name='home'),
     url(r'^register/$', RegisterView.as_view(), name='register'),
+    url(r'^activate/(?P<code>[a-z0-9].*)/$', activate_user_view, name='activate'),
     url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^logout/$', LogoutView.as_view(), name='logout'),
     url(r'^profile-follow/$', ProfileFollowToggle.as_view(), name='follow'),
@@ -39,4 +22,3 @@ urlpatterns = [
     url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
     url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
 ]
-
---
+++ src/muypicky/xurls.py
@@ -0,0 +1,42 @@
+"""muypicky URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.views.generic import TemplateView
+
+# source:  class LoginView https://docs.djangoproject.com/en/1.11/topics/auth/default/#django.contrib.auth.views.LoginView
+
+from django.contrib.auth.views import LoginView, LogoutView
+# https://docs.djangoproject.com/en/1.11/topics/auth/default/#django.contrib.auth.views.LogoutView
+
+from menus.views import HomeView
+from profiles.views import ProfileFollowToggle, RegisterView
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    # url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^register/$', RegisterView.as_view(), name='register'),
+    url(r'^login/$', LoginView.as_view(), name='login'),
+    url(r'^logout/$', LogoutView.as_view(), name='logout'),
+    url(r'^profile-follow/$', ProfileFollowToggle.as_view(), name='follow'),
+    url(r'^u/', include('profiles.urls', namespace='profiles')),
+    url(r'^items/', include('menus.urls', namespace='menus')),
+    url(r'^restaurants/', include('restaurants.urls', namespace='restaurants')),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
+    url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
+]
+
